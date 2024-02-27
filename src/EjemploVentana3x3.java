a8233f6627915dc9de618240d59632e51f060baf
Felipe <lamediatricolor@gmail.com>
2024-02-27 17:42:55 -0500

    Agregando ejemplo de Ventana 3x3

---
+++ src/EjemploVentana3x3.java
@@ -0,0 +1,52 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class EjemploVentana3x3 extends JFrame {
+    JPanel panelMenu;
+    Toolkit pantalla;
+    Dimension misDimensiones;
+    JButton buttonClientes = new JButton("Clientes");
+    JButton buttonFactura = new JButton("Factura");
+    JButton buttonCaja = new JButton("Caja");
+    JButton buttonInventario = new JButton("Inventario");
+    JButton buttonEmpleados = new JButton("Empleados");
+    JButton buttonAdministration = new JButton("Administration");
+    JButton buttonFinanzas = new JButton("Finanzas");
+    JButton buttonAbrirCaja = new JButton("Abrir Caja");
+    JButton buttonExtra = new JButton("");
+
+    public EjemploVentana3x3(){
+        // Establecer las dimensiones de la ventana
+        pantalla = Toolkit.getDefaultToolkit();
+        misDimensiones = pantalla.getScreenSize();
+        setSize(misDimensiones.width,misDimensiones.height);
+
+        // Basic configurations
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setTitle("Sistema de Tienda");
+        setResizable(false);
+
+        // Agregar panel
+        this.panelMenu = new JPanel();
+        this.panelMenu.setLayout(new GridLayout(3, 3));
+
+        // Agregar botones
+        agregarBotones();
+
+        // Agregar botones
+        add(this.panelMenu);
+    }
+
+    public void agregarBotones(){
+        JButton [] botones = {buttonClientes, buttonFactura, buttonCaja, buttonInventario
+        ,buttonEmpleados, buttonAdministration, buttonFinanzas, buttonAbrirCaja, buttonExtra};
+        for (JButton botone : botones) {
+            this.panelMenu.add(botone);
+        }
+    }
+
+    public static void main(String[] args) {
+        EjemploVentana3x3 v = new EjemploVentana3x3();
+        v.setVisible(true);
+    }
+}
