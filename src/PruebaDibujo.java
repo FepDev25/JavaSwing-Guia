f244b84d4da192487c4e4bdedaccbcd1124c0c4e
Felipe <lamediatricolor@gmail.com>
2024-02-27 21:25:28 -0500

    Completando la clase pruebaDibujo con ejemplos de la clase Graphics

--- src/PruebaDibujo.java
+++ src/PruebaDibujo.java
@@ -5,6 +5,8 @@ public class PruebaDibujo {
     public static void main(String[] args) {
         VentanaConDibujos v = new VentanaConDibujos();
         v.setVisible(true);
+        PanelConFiguras panelFig = new PanelConFiguras();
+        v.add(panelFig);
     }
 }
 
@@ -12,7 +14,30 @@ class VentanaConDibujos extends JFrame{
     public VentanaConDibujos(){
         setTitle("Ventana con dibujo");
         setSize(500,500);
-        setLocationRelativeTo(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+}
+
+class PanelConFiguras extends JPanel{
+    public PanelConFiguras(){
+    }
+    @Override
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        // Rectangle: Pos x, pos Y, ancho, alto
+        // g.drawRect(50,50,200,200);
+
+        // Line: Punto Inicial (x, y), Punto Final (x, y)
+        // g.drawLine(100,100,300,200);
+
+        // Arco: X, Y, Ancho arco, Alto arco, angle inicial, alto angle
+        // g.drawArc(50, 100, 100,200, 120, 150);
+
+         // Oval: X, Y, Ancho, Alto,
+        // g.drawOval(100,100,130,130);
+
+        // Oval: X, Y, Ancho, Alto, con color de fondo
+        g.fillOval(100,100,130,130);
+
     }
 }
