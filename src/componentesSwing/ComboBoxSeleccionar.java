2fdca07086cae6b2f2840190bd9c4cac6532fccb
Felipe <lamediatricolor@gmail.com>
2024-03-14 12:20:04 -0500

    Clase para combo box

--- src/componentesSwing/ComboBoxSeleccionar.java
+++ src/componentesSwing/ComboBoxSeleccionar.java
@@ -1,4 +1,29 @@
 package componentesSwing;
 
+import javax.swing.*;
+
 public class ComboBoxSeleccionar {
+    public static void main(String[] args) {
+        VentanaComboBox v = new VentanaComboBox();
+        v.setVisible(true);
+    }
+}
+
+
+class VentanaComboBox extends JFrame{
+    PanelComboBox panel;
+    public VentanaComboBox(){
+        setSize(500,500);
+        setTitle("Combo Box");
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        panel = new PanelComboBox();
+        add(panel);
+    }
+}
+
+class PanelComboBox extends JPanel{
+    public PanelComboBox(){
+
+    }
 }
